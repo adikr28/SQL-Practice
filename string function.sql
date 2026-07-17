1081b878fd3cdc6d6bd9a4730e93cbaae3a11d4e
adikr28 <22BCS50103@cuchd.in>
2026-07-18 02:48:08 +0530

    string function

---
+++ string function.sql
@@ -0,0 +1,26 @@
+select 
+FirstName,
+country,
+concat(FirstName,'=',country) AS namecountry,
+lower(firstName) as lowwercase,
+upper(firstname) as uppercase,
+len(firstname),
+left(firstname,2) as leftside,
+right(firstname,3) as rightside,
+substring(firstname,2,len(firstname))as sub_name
+from sales.customers
+--
+
+
+select
+ '123*456*698' ,
+ REPLACE('123*456*698' ,'*','/')
+   
+
+   select 6.547,
+   round(6.547,2
+   )
+
+   select
+   -15,
+   ABS(-15)
